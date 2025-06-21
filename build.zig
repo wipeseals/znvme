2b74331b1aa9c9e17f20ff33f9e7aa343be577c5
wipeseals <wipeseals@gmail.com>
2025-06-21 13:55:10 +0000

    build: link libc in build process

--- build.zig
+++ build.zig
@@ -64,6 +64,7 @@ pub fn build(b: *std.Build) void {
         .name = "toynvme",
         .root_module = exe_mod,
     });
+    exe.linkLibC();
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
